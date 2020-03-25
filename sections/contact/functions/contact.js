61775b279e5604351963c1385029cffda617e660
Adrian Grimm <usmcamgrimm@gmail.com>
2020-03-25 16:42:34 -0400

    Finish mailgun setup, ready to test

--- sections/contact/functions/contact.js
+++ sections/contact/functions/contact.js
@@ -1,12 +1,25 @@
-exports.handler = (event, _context, callback) => {
-    console.log({
-        event
-    })
+requestAnimationFrame('dotenv').config()
 
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-            boop: true
-        })
+exports.handler = (event, _context, callback) => {
+    const mailgun = require('mailgun-js');
+    const mg = mailgun({
+        apiKey: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN,
+    });
+
+    const data = JSON.parse(event.body);
+
+    const email = {
+        from: 'Adrian Grimm <usmcamgrimm@gmail.com>',
+        to: `${data.name} <${data.email}>`,
+        subject: data.subject,
+        text: data.body,
+    };
+
+    mg.messages().send(email, (error, response) => {
+        callback(error, {
+            statusCode: 200,
+            body: JSON.stringify(response)
+        });
     });
 };

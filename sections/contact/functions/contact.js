27ab7e93155160191b8d0ae8450e16553c220d4c
Adrian Grimm <usmcamgrimm@gmail.com>
2020-03-25 16:12:49 -0400

    get set up for mailgun

---
+++ sections/contact/functions/contact.js
@@ -0,0 +1,12 @@
+exports.handler = (event, _context, callback) => {
+    console.log({
+        event
+    })
+
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+            boop: true
+        })
+    });
+};

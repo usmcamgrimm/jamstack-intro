5bf704bc68c8fc3a37daac7f4d4a361ccaec6bd9
Adrian Grimm <usmcamgrimm@gmail.com>
2020-03-26 12:39:48 -0400

    Add dashboard profiles and router

--- sections/dashboard/src/pages/dashboard.jsx
+++ sections/dashboard/src/pages/dashboard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import Layout from '../components/layout'
+import Profile from '../components/profile'
+import RouteBase from '../components/route-base'
+import RouteSecret from '../components/route-secret'
 
 const Dashboard = () => {
     return (
         <Layout>
+            <Profile />
+            <Router>
+                <RouteBase path="/dashboard/base" />
+                <RouteSecret path="/dashboard/secret" />
+            </Router>
             <p>TODO: create a dashboard</p>
         </Layout>
     );

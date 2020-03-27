7ac7add1afcebddf1eb623fbf313ab783b6a5216
Adrian Grimm <usmcamgrimm@gmail.com>
2020-03-27 17:05:29 -0400

    start gatsby

---
+++ sections/todos/src/pages/index.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import styles from './index.module.css'
+
+export default () => {
+    const [status, setStatus] = useState('loading');
+    const [todos, setTodos] = useState(null);
+    return (
+        <main>
+            <h1 className={styles.heading}>JAMStack Todos</h1>
+            {todos ? (
+                <p>Show Todos</p>
+            ) : (
+                <p className={styles.loading}>Loading Todos</p>
+            )};
+        </main>
+    );
+};

aa8c039d7078a57653548e136fc18b0481b81804
Adrian Grimm <usmcamgrimm@gmail.com>
2020-03-29 14:23:30 -0400

    Add function to create todos

---
+++ sections/todos/functions/create-todo.js
@@ -0,0 +1,38 @@
+const sendQuery = require('./utils/send-query');
+
+const CREATE_TODO = `
+    mutation($text: string!) {
+        createTodo(data: {text: $text, completed: false})
+        {
+            _id
+            text
+            completed
+        }
+    }
+`;
+
+exports.handler = async event => {
+    const {
+        text
+    } = JSON.parse(event.body);
+    const {
+        data,
+        errors
+    } = await sendQuery(CREATE_TODO, {
+        text
+    });
+
+    if (errors) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(errors)
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            newTodo: data.createTodo
+        })
+    };
+}
